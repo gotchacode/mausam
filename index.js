c6b2b655a2b69f08bc16910ac858b0612a88a150
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-01-25 11:24:52 +0530

    working geoIP

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
+#! /usr/bin/env node
 
+const https = require('https');
+const geoipURI = `https://telize.j3ss.co/geoip`;
+
+https.get(geoipURI, (res) => {
+  const { statusCode } = res;
+  let error;
+  if (statusCode !== 200) {
+    error = new Error('Request Failed');
+  }
+
+  if (error) {
+    console.log(error.message);
+    res.resume();
+    return;
+  }
+
+  res.setEncoding('utf8');
+
+  let rawData = '';
+
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      const parsedData = JSON.parse(rawData);
+      console.log(parsedData);
+    } catch(e) {
+      console.log(e.message);
+    }
+  });
+});
+
+
+
+const [,, ...args] = process.argv;
+
+console.log(`Hello World ${args}`);
 
