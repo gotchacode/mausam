846c94a5ef800440e4e9dd1d73d625edf6084f4a
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-02-01 12:41:41 +0530

    update wip for weather data

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 #! /usr/bin/env node
 
 const https = require('https');
+const http = require('http');
 const geoipURI = `https://telize.j3ss.co/geoip`;
+const APIKEY = process.env.WUNDER_KEY;
+
+
+console.log('APIKEY', APIKEY);
+function urlBuilder(type, location) {
+  return `http://api.wunderground.com/api/${APIKEY}/${type}/q/${location.lat},${location.lng}.json`;
+}
+
 
 https.get(geoipURI, (res) => {
   const { statusCode } = res;
@@ -24,7 +33,40 @@ https.get(geoipURI, (res) => {
   res.on('end', () => {
     try {
       const parsedData = JSON.parse(rawData);
-      console.log(parsedData);
+      let location = {
+        lat: parsedData.latitude,
+        lng: parsedData.longitude
+      };
+      const weatherURL = urlBuilder('conditions/forecast/astronomy', location);
+      http.get(weatherURL, (res) => {
+        const { statusCode } = res;
+        let error;
+
+        if (statusCode !== 200) {
+          error = new Error('Request failed to complete');
+        }
+
+        if (error) {
+          console.log(error.message);
+          res.resume();
+          return;
+        }
+
+        res.setEncoding('utf8');
+        let rawWeatherData = '';
+
+        res.on('data', (chunk) => { rawWeatherData += chunk; });
+        res.on('end', () => {
+          try {
+            const weatherData = JSON.parse(rawWeatherData)
+            console.log(weatherData);
+          } catch (e) {
+            console.log(e.message);
+          }
+        });
+
+
+      });
     } catch(e) {
       console.log(e.message);
     }
